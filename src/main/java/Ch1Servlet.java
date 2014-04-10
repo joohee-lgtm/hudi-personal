afe1cda3fdd1bad1a31f6449014378c57daeb011
woonohyo <wonhyo.yi@gmail.com>
2014-04-10 16:44:27 +0900

    Studying HF Servlet

---
+++ src/main/java/Ch1Servlet.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class Ch1Servlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		java.util.Date today = new java.util.Date();
+		out.println("<html>" + "<h1 align=center>Woonohyo\'s HF Chapter1 Servlet</h1>" + "<br>" + today + "</body>" + "</html>");
+		//super.doGet(req, resp);
+	}
+}
+ 
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,18 @@
 <html>
 <body>
-Feeling like getting cancer.
+	<h1 align="center">Beer Selection Page</h1>
+	<form method="POST" action="SelectBeer.do">
+		Select beer characteristics
+		<p>
+			Color: <Select name:"color" size="1">
+				<option value="light">light</option>
+				<option value="amber">amber</option>
+				<option value="brown">brown</option>
+				<option value="dark">dark</option>
+			</Select><br> <br>
+		<center>
+			<input type="SUBMIT">
+		</center>
+	</form>
 </body>
 </html>
