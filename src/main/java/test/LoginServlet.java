6b0dfb9d5fdf38bc10ce2efc5edc5c866af39e9d
aprilchoi <choiapril6@gmail.com>
2014-04-12 02:20:56 +0900

    DB connection done

--- src/main/java/test/LoginServlet.java
+++ src/main/java/test/LoginServlet.java
@@ -2,20 +2,80 @@ package test;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class LoginServlet extends HttpServlet{
+	private Connection conn;
+	private Statement stmt;
+
 	public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException
 	{	
 		PrintWriter out = response.getWriter();
 		String username = request.getParameter("username");
 		out.println("user name: " + username);
+
+		String url = "jdbc:mysql://localhost:3306/collagejam";
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			conn = DriverManager.getConnection(url, "root", "db0000");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if(conn != null) {
+			out.println("connected to db");
+		}
+		else {
+			out.println("failed to connect to db");
+		}
 		
-		User u = new User();
-		Connection c = u.connect();
-		//u.close(c);
+		try {
+			stmt = conn.createStatement();
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		String sql = "select * from user";
+		ResultSet rs = null;
+
+		try {
+			rs = stmt.executeQuery(sql);
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			while(rs.next()) {
+				String id = rs.getString("username");
+				String pw = rs.getString("password");
+				System.out.printf("username: %s  password: %s\n", id, pw);
+			}
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		out.println("closed connection to db");
 	}
 }
--- src/main/java/test/User.java
+++ src/main/java/test/User.java
@@ -3,26 +3,30 @@ package test;
 import java.sql.*;
 
 public class User {
-	private String url = "jdbc:mysql://10.73.45.132/test";
+	private String url = "jdbc:mysql://localhost/collagejam";
 	private String user = "root";
-	private String pw = "leonard911";
+	private String pw = "db0000";
 	private Connection con;
 	
 	public Connection connect(){
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, pw);
 		} catch (ClassNotFoundException e) {
 			System.err.println("Driver Error " + e.getMessage());
-		}
-		System.out.println("Driver Loading success");
-		Connection con = null;
-		
-		try {
-			con = DriverManager.getConnection(url, user, pw);
 		} catch (SQLException e) {
-			System.err.println("Connection Failed: " + e.getMessage());
-			return null;
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+//		System.out.println("Driver Loading success");
+//		Connection con = null;
+//		
+//		try {
+//			con = DriverManager.getConnection(url, user, pw);
+//		} catch (SQLException e) {
+//			System.err.println("Connection Failed: " + e.getMessage());
+//			return null;
+//		}
 		System.out.println("Connection Success");
 		return con;
 	}
