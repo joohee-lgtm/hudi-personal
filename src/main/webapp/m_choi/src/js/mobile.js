b842e17fba385312c182ad8c8067e75c028c74db
aprilchoi <choiapril6@gmail.com>
2014-05-27 14:23:44 +0900

    updated

--- src/main/webapp/m_choi/src/js/mobile.js
+++ src/main/webapp/m_choi/src/js/mobile.js
@@ -3,6 +3,11 @@ oTouch = {
 };
 
 _JE = {
+	getViewport : function() {
+		if (typeof window.innerWidth != 'undefined')
+			var viewPortWidth = window.innerWidth;
+		return viewPortWidth;
+	},
 	getSpecificProperty : function(ele, property) {
 		return window.getComputedStyle(ele).getPropertyValue(property);
 	},
@@ -15,43 +20,39 @@ _JE = {
 	},
 	getElByClass : function(name) {
 		return document.getElementsByClassName(name);
+	},
+	getElById : function(name) {
+		return document.getElementById(name);
+	},
+	toggleContents : function(e) {
+		var tar = e.target;
+		var wrapper = tar.parentNode.parentNode.children[1];
+		var display = _JE.getSpecificProperty(wrapper, "display");
+		wrapper.style.display = (display === 'none') ? 'block' : 'none';
+	},
+	setCSSStyle : function(element, propertyObject) {
+		for(var property in propertyObject) 
+			element.style[property]= propertyObject[property];
 	}
 }
-	
 
 var S_DATA = {
-		WEINRE_TEST_SERVER 	: "10.73.42.167",
+		WEINRE_TEST_SERVER 	: "192.168.0.163",
 		WEINRE_PORT 		: "9999",
 		RIGHT_CARD_LEFT_VAL : 100,
 		IMG_VIEWPORT_RATIO_PORTRAIT	: 0.6,
 		IMG_VIEWPORT_RATIO_LANDSCAPE : 0.8
 }
 
-function toggleContents(e) {
-	var tar = e.target;
-	var wrapper = tar.parentNode.parentNode.children[1];
-	var display = _JE.getSpecificProperty(wrapper, "display");
-	wrapper.style.display = (display === 'none') ? 'block' : 'none';
-}
-
-function getViewport() {
-	var viewPortWidth;
-	if (typeof window.innerWidth != 'undefined')
-		viewPortWidth = window.innerWidth;
-	return viewPortWidth;
-}
-
 function handleTouchstart(e) {
 	oTouch.touchstartX = e.touches[0].clientX;
 	oTouch.touchstartY = e.touches[0].clientY;
-	
 }
 
 function handleTouchmove(e) {
 	var touch = e.touches[0];
-	oTouch.touchX = touch.clientX;
-	oTouch.touchY = touch.clientY;
-	oTouch.nValue = oTouch.touchX - oTouch.touchstartX;
+	var touchX= touch.clientX;
+	oTouch.nValue = touchX - oTouch.touchstartX;
 	oTouch.sDirection = getDirection(oTouch.nValue);
 	if(isInvalidFlicking()) 
 		return false;
@@ -97,12 +98,10 @@ function handleTouchend(e) {
 	var nTmpIndex = oTouch.nIndex;
 	var container = _JE.getElBySel(".container");
 	
-	if(isToRight()) {
+	if(isToRight()) 
 		oTouch.nIndex++;
-	}
-	else {
+	else 
 		oTouch.nIndex--;
-	}
 
 	if(isIndexInRange()) {
 		setPositionWithClassName('about');
@@ -141,9 +140,11 @@ function setPositionWithClassName(classname) {
 		nRightIndex = 0;
 	}
 
-	aChildNodes[nLeftIndex].style.left = S_DATA.RIGHT_CARD_LEFT_VAL * -1 + "%";
+	var left = S_DATA.RIGHT_CARD_LEFT_VAL + "%";
+	console.log(left);
+	aChildNodes[nLeftIndex].style.left = S_DATA.RIGHT_CARD_LEFT_VAL + "%";
 	aChildNodes[nCenterIndex].style.left = "0%";
-	aChildNodes[nRightIndex].style.left = S_DATA.RIGHT_CARD_LEFT_VAL + "%";
+	aChildNodes[nRightIndex].style.left = "100%";
 }
 
 function getDirection() {
@@ -151,7 +152,7 @@ function getDirection() {
 }
 
 function alignJarFramesWithClassName(classname) {
-	var viewPortWidth = getViewport();
+	var viewPortWidth = _JE.getViewport();
 	var aFrames = _JE.getElByClass(classname);
 	var frameWidth = aFrames[0].offsetWidth;
 	var margin = (viewPortWidth - frameWidth) / 2;
@@ -161,21 +162,17 @@ function alignJarFramesWithClassName(classname) {
 		aFrames[i].style.marginRight = margin + 'px';
 	}
 
-	var eleWrapper = getElById('jar-wrapper');
+	var eleWrapper = _JE.getElById('jar-wrapper');
 }
 
-function getElById(name) {
-	return document.getElementById(name);
-}
-
-function adjustFrameWidth(card, container) {
-	var viewPortWidth = getViewport();
+function adjustCardFrameWidth(card, container) {
+	var viewPortWidth = _JE.getViewport();
 	var aFrames = _JE.getElByClass(card);
 	var eleContainer = _JE.getElBySel(container);
-	
-	for(var i = 0; i < aFrames.length; i ++) {
+	var numOfPanels = aFrames.length;
+	for(var i = 0; i < aFrames.length; i ++) 
 		aFrames[i].style.width = viewPortWidth + 'px';
-	}
+	
 	eleContainer.style.width = (viewPortWidth * numOfPanels) + 'px';
 }
 
@@ -188,11 +185,13 @@ function adjustImgWidthOnPortrait(classname) {
     var eleImgLen = eleImg.length;
 
 	for(var i = 0; i < eleImgLen; i ++) {
-		eleImg[i].style.width = imgWidth + 'px';
-		eleImg[i].style.height = imgWidth + 'px';
-		eleImg[i].style.left = '50%';
-		eleImg[i].style.marginLeft = -1 * (imgWidth/2) + 'px';
-		eleImg[i].style.top = '10%';
+		_JE.setCSSStyle(eleImg[i], {
+			width 			: imgWidth + 'px',
+			height 			: imgWidth + 'px',
+			left 			: '50%',
+			'margin-left' 	: -1 * (imgWidth / 2) + 'px',
+			top 			: '10%'
+		});
 	}
 }
 
@@ -204,12 +203,14 @@ function adjustImgWidthOnLandscape(classname) {
 	var eleImg = _JE.getElByClass(classname);
     var eleImgLen = eleImg.length;
     
-	for(var i = 0; i < eleImgLen; i ++) {
-		eleImg[i].style.width = imgWidth + 'px';
-		eleImg[i].style.height = imgWidth + 'px';
-		eleImg[i].style.top = '50%';
-		eleImg[i].style.marginTop = -1 * (imgWidth/2) + 'px';
-		eleImg[i].style.left = '10%';
+	for(var i = 0; i < eleImgLen; i ++) {		
+		_JE.setCSSStyle(eleImg[i], {
+			width 			: imgWidth + 'px',
+			height 			: imgWidth + 'px',
+			top 			: '50%',
+			'margin-top' 	: -1 * (imgWidth / 2) + 'px',
+			left 			: '10%'
+		});
 	}
 }
 
@@ -229,45 +230,59 @@ function getScriptSrc() {
 	return "http://" + S_DATA.WEINRE_TEST_SERVER + ":" + S_DATA.WEINRE_PORT + "/target/target-script-min.js#anonymous";
 }
 
-function registerEvents() {
-	var aToggler = _JE.getElByClass('toggler');
+function initVariableForNumOfCard() {
+	S_DATA.NUM_CARDS = _JE.getElBySelWithParent(".about", ".container").length;
+	S_DATA.LAST_CARD_IDX = S_DATA.NUM_CARDS - 1;
+}
 
-	for(var i = 0; i < aToggler.length; i ++) {
-		aToggler[i].addEventListener('touchstart', toggleContents, false);
-	}
+function initVariables() {
+	initVariableForNumOfCard();
+}
 
-	var flickView = getElById('flickView');
+function registerFlickingEvent(eleId) {
+	var flickView = _JE.getElById(eleId);
 	flickView.addEventListener('touchstart', handleTouchstart, false);
 	flickView.addEventListener('touchmove', handleTouchmove, false);
 	flickView.addEventListener('touchend', handleTouchend, false);
 }
 
-// function showSameIntroductionCard() {
-// 	var ele = getElById('flickView');
-// 	var display = getSpecificProperty(ele, "display");
-// 	if(display !== 'none') {
-// }
-// }
+function registerToggleMenuEvent(classname) {
+	var aToggler = _JE.getElByClass(classname);
+	for(var i = 0; i < aToggler.length; i ++)
+		aToggler[i].addEventListener('touchstart', _JE.toggleContents, false);
+}
 
-function initVariables() {
-	S_DATA.NUM_CARDS = _JE.getElBySelWithParent(".about", ".container").length;
-	S_DATA.LAST_CARD_IDX = S_DATA.NUM_CARDS - 1;
-	// S_DATA.LEFT_CARD_IDX = S_DATA.NUM_CARDS - 1;
-	// S_DATA.RIGHT_CARD_IDX = S_DATA.NUM_CARDS + 1;
+function registerEvents() {
+	registerToggleMenuEvent('toggler');
+	registerFlickingEvent('flickView');
+}
+
+function init() {
+	adjustCardFrameWidth('about', ".container");
+	adjustImgWidthOnPortrait('aboutImg');
+	adjustImgWidthOnLandscape('aboutImg');
+	alignJarFramesWithClassName('jamjar');
+	initVariables();
 }
 
 window.addEventListener('load', function() {
 	registerEvents();
-	alignJarFramesWithClassName('jamjar');
-	initVariables();
-	adjustImgWidthOnPortrait('aboutImg');
-	adjustImgWidthOnLandscape('aboutImg');
+	init();
 	//addScriptForWeinre();
 }, false);
 
 window.addEventListener('orientationchange', function() {
 	alignJarFramesWithClassName('jamjar');
-	adjustFrameWidth('about', ".container");
+	adjustCardFrameWidth('about', ".container");
+	
 	adjustImgWidthOnPortrait('aboutImg');
 	adjustImgWidthOnLandscape('aboutImg');
 }, false);
+
+window.mFlicking = (function() {
+	var _static = {
+			RIGHT_CARD_LEFT_VAL : 100
+	}
+})();
+
+
