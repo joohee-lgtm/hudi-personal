a4363135e1cef8940a25745d7799e57f265f8d06
aprilchoi <choiapril6@gmail.com>
2014-06-06 14:48:30 +0900

    file name changed

--- src/main/java/net/collagejam/obj/Photo.java
+++ src/main/java/net/collagejam/obj/Photo.java
@@ -1,11 +1,11 @@
 package net.collagejam.obj;
 
-public class PhotoList {
+public class Photo {
 	private int j_id;
 	private int index;
 	private String url;
 	
-	public PhotoList(int j_id, int index, String url) {
+	public Photo(int j_id, int index, String url) {
 		this.j_id	= j_id;
 		this.index 	= index;
 		this.url	= url;
