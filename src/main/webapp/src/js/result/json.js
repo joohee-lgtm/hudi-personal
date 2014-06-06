22ec6d91a285ccea6d348585e3c8ef70d5fac731
aprilchoi <choiapril6@gmail.com>
2014-06-06 14:35:07 +0900

    jamjar 정보 화면에 표시 완료

--- src/main/webapp/src/js/result/json.js
+++ src/main/webapp/src/js/result/json.js
@@ -1,5 +1,6 @@
 var head 		= document.getElementById('data');
 var child 		= document.createElement('div');
+child.id		= "jamjar";
 var json 		= JSON.stringify(jamjar);
 child.innerHTML = json; 
 head.appendChild(child);
