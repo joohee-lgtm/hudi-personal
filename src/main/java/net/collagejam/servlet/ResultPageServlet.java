5ff7f314800a22b41179a897b632fac896093b75
aprilchoi <choiapril6@gmail.com>
2014-06-06 18:25:38 +0900

    photo list 가져오기 성공

--- src/main/java/net/collagejam/servlet/ResultPageServlet.java
+++ src/main/java/net/collagejam/servlet/ResultPageServlet.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.aprilchoi.user.JamjarDao;
-import net.aprilchoi.user.PhotoListDao;
+import net.aprilchoi.user.PhotoDao;
 import net.collagejam.obj.JamJar;
 
 import com.google.gson.Gson;
@@ -21,7 +21,7 @@ import com.google.gson.Gson;
 
 public class ResultPageServlet extends HttpServlet{
 	private JamjarDao jdao;
-	private PhotoListDao pldao;
+	private PhotoDao pdao;
 	
 	private Connection getConnection() throws Exception {
 		Class.forName("com.mysql.jdbc.Driver");
@@ -44,7 +44,7 @@ public class ResultPageServlet extends HttpServlet{
 		}
 		
 		jdao	= new JamjarDao(conn);
-		pldao	= new PhotoListDao();
+		pdao	= new PhotoDao(conn);
 		
 		try {
 			selected = jdao.selectByJarId(jid);
@@ -52,12 +52,14 @@ public class ResultPageServlet extends HttpServlet{
 			e.printStackTrace();
 		}
 		
-//		try {
-//			aUrl = pldao.getUrlList(jid);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			aUrl = pdao.selectListOfPhotosByJarId(jid);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		System.out.println(aUrl);
 		String jarJson = gson.toJson(selected);
 		request.setAttribute("id", jid);
 		request.setAttribute("jamjar", jarJson);
