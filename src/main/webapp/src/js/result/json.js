9d66e1e1764447b561537b217aff496d6a869d93
aprilchoi <choiapril6@gmail.com>
2014-06-07 16:37:46 +0900

    json result not overflow

--- src/main/webapp/src/js/result/json.js
+++ src/main/webapp/src/js/result/json.js
@@ -1,6 +1,5 @@
 var head 		= document.getElementById('data');
-var child 		= document.createElement('div');
-child.id		= "jamjar";
+var child 		= document.createElement('span');
 var json 		= JSON.stringify(jamjar);
 child.innerHTML = json; 
 head.appendChild(child);
