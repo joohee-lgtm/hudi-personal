dbe6bc188b0e42fcd885e5aac93c9444b78c9953
aprilchoi <choiapril6@gmail.com>
2014-06-09 11:48:23 +0900

    insert error resolved

--- src/main/java/net/collagejam/web/CreateJarController.java
+++ src/main/java/net/collagejam/web/CreateJarController.java
@@ -1,17 +1,8 @@
 package net.collagejam.web;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import net.collagejam.model.DBSetting;
 
@@ -76,6 +67,7 @@ public class CreateJarController {
 			}
 		}
 	}
+
 	
 	void insertInfoSql(Statement stmt, int userid, String title, String description, String bgm, String thumbnail, String bgmStart, String bgmEnd, String spi){
 		String q = "\"";
