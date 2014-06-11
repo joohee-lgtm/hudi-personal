fb4ddd1f2e1d96f564f375696b50974fe90064d5
kimjoohwee <kjhwee91@gmail.com>
2014-06-11 15:52:10 +0900

    slidefix에 있던 추가 파일들

---
+++ src/main/webapp/src/js/result/bgm.js
@@ -0,0 +1,39 @@
+/* youtube api 시작 */
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var done = false;
+var player;
+var vdata;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+      height: '300',
+      width: '400',
+      videoId: jamjar.bgm_url,
+      playerVars: {
+    	  'autoplay': 0, 
+    	  'controls': 0, 
+    	  'showinfo' : 0, 
+    	  'showsearch' : 0, 
+    	  'modestbranding' : 0,
+    	  'disablekb' : 0}
+    });
+    jObjInfo.getBgmInfo();
+}
+
+function onPlayerReady(evt) {
+    evt.target.playVideo();
+}
+
+function onPlayerStateChange(evt) {
+    if (evt.data == YT.PlayerState.PLAYING && !done) {
+        setTimeout(stopVideo, 6000);
+        done = true;
+    }
+}
+
+function stopVideo() {
+    player.stopVideo();
+}
---
+++ src/main/webapp/src/js/result/info.js
@@ -0,0 +1,38 @@
+function jarInfoManager(jamjar){
+	this.user = jamjar.u_id;
+	this.title = jamjar.title;
+	this.desc = jamjar.description;
+	this.date = jamjar.date_created;
+	this.speed = jamjar.sec_per_img * 10;
+	this.m_title = "";
+}
+
+
+jarInfoManager.prototype = {
+	getBgmInfo : function(){
+		var ytframe = document.getElementById("player");
+		var that = this;
+		ytframe.addEventListener('load', function(){
+			that.m_title = player.getVideoData().title;
+			that.setInfoView();
+		}, false);
+	},
+	
+	setInfoView : function(){
+		this.setData(0, this.user);
+		this.setData(1, this.title);
+		this.setData(2, this.desc);
+		this.setData(3, this.m_title);
+		this.setData(4, this.date);
+	},
+
+	setData : function(num, v){
+		var info = document.getElementById("info");
+		var li = info.getElementsByClassName("v");
+		li[num].innerHTML = v;
+	}
+}
+
+
+var jObjInfo = new jarInfoManager(jamjar);
+
