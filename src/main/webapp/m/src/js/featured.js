011d5b9db7dfacdaa13eb0a9a360ddf2f6551497
kimjoohwee <kjhwee91@gmail.com>
2014-06-12 14:30:43 +0900

    featured css 수정

--- src/main/webapp/m/src/js/featured.js
+++ src/main/webapp/m/src/js/featured.js
@@ -15,35 +15,55 @@ featuredGroupManager.prototype = {
 		var ul = document.getElementsByTagName("ul")[0];
 		for (var i=0; i<showarr.length ; i++){
 			var node = this.makeJarObj.cr(showarr[i]);
+			this.addEvent(node);
 			ul.appendChild(node);
 		}
 	},
 	
+	addEvent : function(node){
+		var img = node.getElementsByTagName("img")[0];
+
+		img.addEventListener("load", function(){
+			var _width = window.getComputedStyle(node).width;
+			img.style.width = _width;
+			console.log(img);
+		},false);
+		
+		node.addEventListener("click", function(){
+			var url = "/collageJam/m/result?id=" +node.id;
+			window.location = url;
+		}, false);
+	},
+	
 	makeJarObj : {
 		cr : function(jarobj){
-			var li =  document.createElement("li");
-			var titlespan = this._span(jarobj.title);
-			var descspan = this._span(jarobj.description);
-			var divimg = this._img(jarobj.tb_url);
-			li.innerHTML = titlespan + divimg + descspan;
-			return li;
+			var _li =  document.createElement("li");
+			var titlespan = this.getSpan(jarobj.title);
+			var descspan = this.getSpan(jarobj.description);
+			var divimg = this.getImg(jarobj.tb_url);
+			_li.innerHTML = titlespan + divimg + descspan;
+			_li.id = jarobj.j_id;
+			return _li;
 		},
 		
-		_span : function(text){
-			var span = "<span>" + text + "</span>"
-			return span;
+		getSpan : function(text){
+			var _span = "<span>" + text + "</span>"
+			return _span;
 		},
 		
-		_img : function(url){
-			var img = "<div><img src=\"" + url + "\"></div>";
-			console.log(img);
-			return img;
+		getImg : function(url){
+			var _img = "<div><img src=\"" + url + "\"></div>";
+			return _img;
 		}
 	}
 }
 
-var fgm = new featuredGroupManager(data, 5);
-var button = document.getElementsByTagName("button")[0];
-button.addEventListener("click", function(){
+
+window.addEventListener("load",function(){
+	var fgm = new featuredGroupManager(data, 5);
 	fgm.setJarShow();
-}, false);
+	var button = document.getElementsByTagName("button")[0];
+	button.addEventListener("click", function(){
+		fgm.setJarShow();
+	}, false);
+},false);
